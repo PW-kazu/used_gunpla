b96a34ad592dc20c405dd3b4d9b35bf951a03b5f
PW kazu <komura_jutasrou@icloud.com>
2022-12-21 20:29:54 +0900

    訂正

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,6 +14,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call(CustomerFactory::class);
+        $this->call(UsersTableSeeder::class);
     }
 }
