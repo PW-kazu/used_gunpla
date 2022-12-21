a4440a8d43857cbcc25a7a8eaba405c955ec9a6a
PW kazu <komura_jutasrou@icloud.com>
2022-12-21 18:01:37 +0900

    corrrected

---
+++ database/factories/CustomerFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+
+class CustomerFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'name' => $this->faker->name,
+             'email' => $this->faker->email,
+             'email_verified_at'=> $this->faker->safeEmail, 
+             'password' => $this->faker->password, 
+        ];
+    }
+}
