812650f59d3ede2850885a1e5014afc6a6b283fe
PW kazu <komura_jutasrou@icloud.com>
2023-01-19 18:27:02 +0900

    修正

--- resources/js/app.js
+++ resources/js/app.js
@@ -8,12 +8,3 @@ library.add(faSearch, faAddressCard, faStoreAlt, faShoppingBag, faSignOutAlt, fa
 
 dom.watch();
 
-document.querySelector('.image-picker input')
-  .addEventListener('change', (e) => {
-    const input = e.target;
-    const reader = new FileReader();
-    reader.onload = (e) => {
-      input.closest('.image-picker').querySelector('img').src = e.target.result
-    };
-    reader.readAsDataURL(input.files[0]);
-  });
